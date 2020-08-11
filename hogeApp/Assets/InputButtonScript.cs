fae4983dc4a88afa17aeed363476a62247a5d908
github tamayose-y <tamayose_yur@msoft.co.jp>
2020-08-11 15:12:20 +0900

    リスト画面作成(list) 画面遷移ができるようになった

---
+++ hogeApp/Assets/InputButtonScript.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+ 
+public class InputButtonScript : MonoBehaviour {
+ 
+    public void OnClickInputButton()
+    {
+        SceneManager.LoadScene("list");
+    }
+ 
+}
